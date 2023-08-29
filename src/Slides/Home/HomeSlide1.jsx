1a65982741b6e03508128061833cc0799d9da808
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-08-29 18:29:20 +0200

    ajout de design responsive pour tablette

--- src/Slides/Home/HomeSlide1.jsx
+++ src/Slides/Home/HomeSlide1.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./HomeSlide1.css";
 import HomePopup from "../../Components/HomePopup/HomePopup";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import RightArrow from "../../assets/icons/iconmonstr-arrow-down-thin.svg";
 import { useMediaQuery } from "react-responsive";
 
 export default function HomeSlide1({ state }) {
-  const isMobile = useMediaQuery({ maxWidth: 950 }); // Par exemple, considérons < 767px comme mobile
+  const isMobile = useMediaQuery({ minWidth: 950 }); // Par exemple, considérons < 767px comme mobile
+  const isSmallDesktop = useMediaQuery({ minWidth: 1680 });
+  const isDesktop = useMediaQuery({ minWidth: 1920 });
 
   const [animHomePopup, setAnimHomePopup] = useState(false);
   const [activeArrow, setActiveArrow] = useState(false);
   const [newStyle, setNewStyle] = useState({});
   const [portfolioClass, setPortfolioClass] = useState("");
   const [polygons, setPolygons] = useState([false, false, false, false, false]);
-
+  const activeHomeTitle = useRef();
   const handleAnimHeader = () => {
-    const newTitleStyle = isMobile ? { fontSize: "57px", fontWeight: "700" } : { fontSize: "230px", fontWeight: "700" };
+    const newTitleStyle = { fontSize: "230px", fontWeight: "700" };
+    const styleMobile = { fontSize: "57px", fontWeight: "700" };
+    const styleSmallDesktop = { fontSize: "180px", fontWeight: "700" };
     setTimeout(() => {
       setAnimHomePopup(!animHomePopup);
     }, 2000);
@@ -49,8 +53,19 @@ export default function HomeSlide1({ state }) {
     }, 6100);
 
     setTimeout(() => {
-      setNewStyle(newTitleStyle);
+      activeHomeTitle.current.classList.add("active-home-title");
     }, 300);
+
+    // if (isSmallDesktop) {
+    //   setTimeout(() => {
+    //     setNewStyle(styleSmallDesktop);
+    //   }, 300);
+    // }
+    // if (isDesktop) {
+    //   setTimeout(() => {
+    //     setNewStyle(newTitleStyle);
+    //   }, 300);
+    // }
 
     return () => {
       timeouts.forEach((timeout) => clearTimeout(timeout));
@@ -64,7 +79,7 @@ export default function HomeSlide1({ state }) {
         {polygons.map((polygon, index) => (
           <div key={index} className={polygon ? `polygon${index + 1}` : ""} />
         ))}
-        <h1 className="home-title" style={newStyle}>
+        <h1 className="home-title" ref={activeHomeTitle}>
           <span className="span-home-title ">D</span>
           <span className="span-home-title">e</span>
           <span className="span-home-title">v</span>
